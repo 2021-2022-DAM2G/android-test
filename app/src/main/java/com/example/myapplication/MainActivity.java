9c3e6630a678212ef6f0a4eed3f653e00506f1bb
mikelsanvi <mikelsanvi@gmail.com>
2022-03-17 10:58:49 +0100

    fixed

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.iesfm.library.Book;
 import org.iesfm.library.client.BookClient;
@@ -16,55 +15,43 @@ import java.io.StringWriter;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private BooksApi booksApi = new BookClient(new RestTemplate(), "http://10.0.2.2:8080");
 
-    List<Book> books;
+    private TextView msg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-               test();
-            }
-        });
-        t.start();
-        try {
-            t.join();
-        } catch (Exception e1) {
-
-        }
-        TextView msg = findViewById(R.id.msg);
-
-
-        for (Book book : books) {
-            msg.append(book.toString());
-        }
-
+        msg = findViewById(R.id.msg);
+        // Ejecutamos el loadBooks en otro thread
+        new Thread(() -> loadBooks()).start();
     }
 
 
-    public void test() {
-        TextView msg = findViewById(R.id.msg);
+    public void loadBooks() {
         try {
-            BooksApi booksApi = new BookClient(new RestTemplate(), "http://10.0.2.2:8080");
 
-            books = booksApi.getBooks(null, null);
+            List<Book> books = booksApi.getBooks(null, null);
 
+            // runOnUiThread(() -> {
+            msg.post(() -> {
+                for (Book book : books) {
+                    msg.append(book.toString());
+                    msg.append("\n");
+                }
+            });
 
         } catch (Exception e) {
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            e.printStackTrace(pw);
-            msg.setText(sw.toString());
-            try {
-                Thread.sleep(1000000000);
-            } catch (Exception e1) {
 
-            }
+            msg.post(() -> {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                e.printStackTrace(pw);
+                msg.setText(sw.toString());
+            });
         }
     }
 }
